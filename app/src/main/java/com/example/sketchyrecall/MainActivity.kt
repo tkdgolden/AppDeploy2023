f9144b926c1e0782a5cbbfe0d5310a0da9c7d80c
tkdgolden <olivia.golden.w@gmail.com>
2023-11-19 15:04:54 -0600

    git

--- app/src/main/java/com/example/sketchyrecall/MainActivity.kt
+++ app/src/main/java/com/example/sketchyrecall/MainActivity.kt
@@ -7,11 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontLoadingStrategy
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import com.example.compose.SketchyRecallTheme
 
 class MainActivity : ComponentActivity() {
