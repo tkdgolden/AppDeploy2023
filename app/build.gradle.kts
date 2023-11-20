d32ef24f3427cafbb4055f9d7d1e4784b1fba4d7
tkdgolden <olivia.golden.w@gmail.com>
2023-11-19 20:33:07 -0600

    gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -61,6 +62,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
     implementation ("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.05.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
